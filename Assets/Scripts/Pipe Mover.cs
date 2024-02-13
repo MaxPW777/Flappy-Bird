cd2ab32f8a9e4226b1ea4ad3a3a83c3e053cb93d
Maximilian <mpinderwhite@gmail.com>
2024-02-13 11:25:48 +0100

    [+] added pipe movement

--- Assets/Scripts/Pipe Mover.cs
+++ Assets/Scripts/Pipe Mover.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PipeMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 newPosition = transform.position;
+        newPosition.x -= 3 * Time.deltaTime;
+        transform.position = newPosition;
+        if (newPosition.x <= -3.3){
+            transform.position = new Vector2(3.2f,Random.value*-6-1);
+        }
     }
 }
